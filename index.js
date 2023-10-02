8e92e4a7e4acef60584cf4e078edc2849aba5f3f
Kush Kaushik <kushkaushik271@gmail.com>
2023-10-02 17:25:19 +0530

    h

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+
+
+
+app.get('/data' , (req , res) =>{
+    res.json({
+        msg : "hellow mr kush kaushik"
+    })
+})
+
+
+
+
+app.listen(3000 , () =>{
+    console.log("Hey User how are you")
+})
